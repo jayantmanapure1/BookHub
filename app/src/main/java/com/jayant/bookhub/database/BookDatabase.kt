287359cac6e75b599af8afeda0ca131ab3de52dc
001jayant <jayantmanapure1@gmail.com>
2022-01-08 13:00:32 +0530

    r1 commit

---
+++ app/src/main/java/com/jayant/bookhub/database/BookDatabase.kt
@@ -0,0 +1,13 @@
+package com.jayant.bookhub.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+
+@Database(entities = [BookEntity::class], version = 1)
+abstract class BookDatabase: RoomDatabase() {
+
+    abstract fun bookDao(): BookDao
+
+}
+
